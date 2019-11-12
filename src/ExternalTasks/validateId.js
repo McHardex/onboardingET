8109c9ae4de1215e52c04cb1759dc1296d27a96a
mchardex <bukunmiadebisi@gmail.com>
2019-11-12 16:07:32 +0100

    persist external tasks in db

--- src/ExternalTasks/validateId.js
+++ src/ExternalTasks/validateId.js
@@ -1,14 +1,20 @@
 import { client } from '../helpers/client';
 import { errorHandler } from '../helpers/errorHandler';
-import saveToDb from '../models';
+import db from '../models';
 
 const validateId = {
   validate: () => {
     client.subscribe("Validate ID", async function ({ task, taskService }) {
       try {
-        await saveToDb('validateId', task);
+        const userDetails = task.variables.getAll();
+
+        await db.validateId.create({
+          processId: task.processInstanceId,
+          userDetails
+        });
         await taskService.complete(task);
       } catch(error) {
+        console.log(error.message, 'error in validate id');
         errorHandler(error, taskService, task);
       }
     });
